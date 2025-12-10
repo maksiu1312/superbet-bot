b5e7bec34de599228ac1343a37f10a0a0429faf7
maksiu1312 <maxpro6yt@gmail.com>
2025-12-10 23:52:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,17 +5,22 @@ from discord.ext import commands, tasks
 TOKEN = os.getenv("TOKEN")
 CHANNEL_ID = 1448439389955948544  # ID kanału na Discordzie
 
-bot = commands.Bot(command_prefix="!")
+# Ustaw intents
+intents = discord.Intents.default()
+intents.message_content = True  # potrzebne do czytania treści wiadomości
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Zalogowano jako {bot.user}')
+    print(f'Logged in as {bot.user}')
     wysylaj_analizy.start()
 
 @tasks.loop(minutes=60)  # co godzinę
 async def wysylaj_analizy():
     channel = bot.get_channel(CHANNEL_ID)
-    analiza = "NBA: Lakers vs Celtics → zwycięzca: Lakers, Over/Under: 210, BTTS: Tak"
-    await channel.send(analiza)
+    if channel is not None:
+        analiza = "NBA: Lakers vs Celtics → winner: Lakers, Over/Under: 210, BTTS: Yes"
+        await channel.send(analiza)
 
 bot.run(TOKEN)
